b7052aef3a90f68119aaf0eb0a9bfa460387b23d
alexander-svendsen <alexander.svendsen@bekk.no>
2015-09-27 15:25:43 +0200

    Cleanup. should only contain the skeleton

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-let five = require('johnny-five');
-let firebase = require('devices-core').firebase;
-let board = new five.Board();
+import { firebase, five } from 'devices-core';
+const TEAM = 'my-awsome-team-name';
+const fb = new firebase(TEAM);
+const board = new five.Board();
 
 board.on("ready", function() {
-    // https://github.com/rwaldron/johnny-five/wiki/Led 
-    let led = new five.Led(11);
-    led.on();
-    led.brightness(2);
-
-    firebase.send('boot', {'name': 'arduino1', 'message': 'hello!'});
-
-    firebase.on('light', function(v) {
-        v = v*(100.0/1023.0);
-        led.brightness(v);
-        console.log(v);
-    });
 
 });
-
-
-
-function toggleLed(led) {
-    let light = true;
-    led.on();
-
-    return function() {
-        light = !light;
-        light ? led.on() : led.off();
-        return light;
-    };
-}

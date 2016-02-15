8eaf7b44b2a477b2fce0b916cec03b835017cf92
Birgitte Haavardsholm <birgitte__89@hotmail.com>
2016-02-15 19:59:21 +0100

    change project

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 import { firebase, five } from 'devices-core';
-const TEAM = 'my-awsome-team-name';
+
+const TEAM = 'laser-target';
 const fb = new firebase(TEAM);
 const board = new five.Board();
 
 board.on("ready", function() {
 
+  const photoresistor = new five.Sensor({
+    pin: "A2", 
+    freq: 100
+  });
+
+  let running = false;
+  let count = 0;
+  let hits = 0;
+
+ 
+    
+  fb.on('laser-tag',  'users/laser-tagger', function(value){
+  	if(value){
+  		running = true;
+  		count = 0;
+  		hits = 0;
+  	}else{
+	  	running = false;
+	    fb.send('stats', { "count" : count, "hits" : hits});
+	  	fb.send('stats', null);
+  	}
+  });
+
+  photoresistor.on("data", function() {
+    if(running){
+   		console.log(this.value);
+    	count++;
+    	if(this.value > 600){
+    		hits++;
+    	}
+    }
+  });
 });
